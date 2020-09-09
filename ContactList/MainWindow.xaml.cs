68f8c9fbb342b2b2d7a4d69ea9fc244299a50d53
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-09 13:28:28 +0200

    ContactList w add to .txt

---
+++ ContactList/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clearfield() 
+        {
+            TbFirstname.Text = "";
+            TbLastname.Text = "";
+        
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = new Contact(TbFirstname.Text, TbLastname.Text);
+
+            try
+            {
+                lbContactlist.Items.Add(contact.FullName);
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C:\workerservice\Contacts\WriteLines2.txt", true)) 
+                {
+                    file.WriteLine(contact.FullName);
+                }
+            }
+            catch
+            {
+            }
+            finally 
+            {
+                Clearfield();
+            
+            }
+
+
+
+        }
+
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lbContactlist.Items.Remove(lbContactlist.Items[lbContactlist.SelectedIndex]);
+            }
+            catch
+            {
+
+                
+            }
+        }
+    }
+}
